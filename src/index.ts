0ce5815e08d9344404b5b3fcaa1d75f33e2dfb09
Augusto Otturi <augustotturi.dev@gmail.com>
2023-03-12 14:14:19 -0600

    First test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
-import { removeCard, sortCardRandomly } from '@helpers/index';
+//CLASS
 
-import './styles/global.css';
+class Card {
+    id: number;
+    name: string;
+    ap: number;
+    hp: number;
 
-removeCard();
-sortCardRandomly();
+    constructor(id: number, name: string, ap: number, hp: number) {
+        (this.id = id), (this.name = name), (this.ap = ap), (this.hp = hp);
+    }
+
+    showInfo() {
+        return `Id:${this.id}\nName: ${this.name}\n AP: ${this.ap}\n HP:${this.hp}`;
+    }
+}
+
+const card1 = new Card(1, 'Jhon', 10, 15);
+const card2 = new Card(2, 'Lucia', 14, 8);
+const card3 = new Card(3, 'Spikear', 13, 10);
+const card4 = new Card(4, 'Lisa', 17, 5);
+
+console.log(card1.showInfo());
+console.log(card2.showInfo());
+console.log(card3.showInfo());
+console.log(card4.showInfo());
+
+function startBattle() {
+    alert("El ganador es player 1");
+}
+
+const btnBATTLE = document.querySelector('#btnStart');
+btnBATTLE?.addEventListener('click', () => {
+    startBattle();
+});
