9f4dd4017cd8ff46780f133a45c9ce76c396f120
Manuel Casares <manuel.casares@mdevz.com>
2023-03-17 18:33:27 -0300

    husky, lint-staged setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
-import { Card, ManuCard } from '@models/index';
+// import { Card, ManuCard } from '@models/index';
 import { $ } from './helpers';
-
-const card1 = new Card(1, 'Jhon', 10, 15);
-const card2 = new Card(2, 'Lucia', 14, 8);
-
-const manuCard1 = new ManuCard({ name: 'Manu', hp: 100, ap: 50 });
-
-console.log(card1.showInfo());
-console.log(card2.showInfo());
 
 function startBattle() {
   alert('El ganador es player 1');
